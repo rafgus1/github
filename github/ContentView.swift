4314417a2c8f1e8ce22884e38f246943c5da0822
Rafał Michałowski <rafgus1@wp.pl>
2022-10-15 16:57:10 +0200

    Initial Commit

---
+++ github/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  github
+//
+//  Created by Rafał Michałowski on 15/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Rafał Michałowski")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by Rafał Michałowski on 15/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
